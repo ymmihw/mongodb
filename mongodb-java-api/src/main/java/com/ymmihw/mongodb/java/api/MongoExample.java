d776ba430d01f1c1fa6d586aba2ffa1a8cdf3175
wanghaoming <whimmy@126.com>
2018-04-11 16:08:32 +0800

    java-api

---
+++ mongodb-java-api/src/main/java/com/ymmihw/mongodb/java/api/MongoExample.java
@@ -0,0 +1,57 @@
+package com.ymmihw.mongodb.java.api;
+
+import java.util.function.Consumer;
+import org.bson.Document;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoExample {
+  public static void main(String[] args) {
+
+    MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+    MongoDatabase database = mongoClient.getDatabase("myMongoDb");
+
+    // print existing databases
+    mongoClient.listDatabaseNames().forEach((Consumer<String>) System.out::println);
+
+    System.out.println("==============");
+
+    database.drop();
+
+    database.createCollection("customers");
+
+    // print all collections in customers database
+    database.listCollectionNames().forEach((Consumer<String>) System.out::println);
+
+    // create data
+    MongoCollection<Document> collection = database.getCollection("customers");
+    Document document = new Document();
+    document.put("name", "Shubham");
+    document.put("company", "Baeldung");
+    collection.insertOne(document);
+
+    // update data
+    BasicDBObject query = new BasicDBObject();
+    query.put("name", "Shubham");
+    BasicDBObject newDocument = new BasicDBObject();
+    newDocument.put("name", "John");
+    BasicDBObject updateObject = new BasicDBObject();
+    updateObject.put("$set", newDocument);
+    collection.updateOne(query, updateObject);
+
+    // read data
+    BasicDBObject searchQuery = new BasicDBObject();
+    searchQuery.put("name", "John");
+    FindIterable<Document> cursor = collection.find(searchQuery);
+    cursor.forEach((Consumer<Document>) System.out::println);
+
+    // delete data
+    BasicDBObject deleteQuery = new BasicDBObject();
+    deleteQuery.put("name", "John");
+    collection.deleteOne(deleteQuery);
+  }
+}
